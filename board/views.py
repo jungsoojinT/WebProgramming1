2afe08427b23e55786b3a7877e16bd3c600a5112
jungsoojin <jdyun12000@sdhs.gwe.hw.kr>
2023-10-14 15:58:25 +0900

    질문목록과 질문 상세 기능

--- board/views.py
+++ board/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 
 def index(request):
@@ -9,3 +9,9 @@ def index(request):
     question_list = Question.objects.order_by("-create_date")
     context = {'question_list':question_list}
     return render(request, 'board/question_list.html', context)
+
+def detail(request, question_id):
+    """내용 출력"""
+    question = get_object_or_404(Question, pk=question_id) #URL로 매핑 할때 저장된 id전달
+    context = {'question':question}
+    return render(request, 'board/question_detail.html', context) # context에 있는 Question 모델 데이터를 html 파일에 적용해 HTML 코드로 변환
